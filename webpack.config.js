1070ac475d3d2667b6d256282997dd0bc6b05317
jrj-sys <jamesjamel99@gmail.com>
2022-06-07 18:41:17 -0500

    idk

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
+  devtool: 'eval-source-map',
   entry: './assets/js/script.js',
   output: {
-    path: path.join(__dirname + '/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
   },
   plugins: [
